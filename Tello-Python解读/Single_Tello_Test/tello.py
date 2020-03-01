99dcef0b73ba36e55a57b3a4fea173400aa186c6
fanuy6662 <shangyif6004@foxmail.com>
2020-03-01 13:55:48 +0800

    Tello_Test“部分分析完成 ”

--- Tello-Python解读/Single_Tello_Test/tello.py
+++ Tello-Python解读/Single_Tello_Test/tello.py
@@ -9,11 +9,12 @@ class Tello:
         self.local_ip = ''
         self.local_port = 8889
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # socket for sending cmd
-        self.socket.bind((self.local_ip, self.local_port))
+        self.socket.bind((self.local_ip, self.local_port))  #套绑定IP和端口
 
         # thread for receiving cmd ack
         self.receive_thread = threading.Thread(target=self._receive_thread)
-        self.receive_thread.daemon = True
+        #调用线程模块：target 是函数名字，需要调用的函数；
+        self.receive_thread.daemon = True #主线程运行结束时不对这个子线程进行检查而直接退出
         self.receive_thread.start()
 
         self.tello_ip = '192.168.10.1'
@@ -33,23 +34,24 @@ class Tello:
         :param ip: (str) the ip of Tello
         :return: The latest command response
         """
-        self.log.append(Stats(command, len(self.log)))
+        self.log.append(Stats(command, len(self.log))) #添加状态到日志
 
-        self.socket.sendto(command.encode('utf-8'), self.tello_adderss)
+        self.socket.sendto(command.encode('utf-8'), self.tello_adderss)  #命令发送至无人机
         print 'sending command: %s to %s' % (command, self.tello_ip)
 
         start = time.time()
-        while not self.log[-1].got_response():
+        while not self.log[-1].got_response(): #state.py中返回是否有回应的状态
             now = time.time()
             diff = now - start
             if diff > self.MAX_TIME_OUT:
-                print 'Max timeout exceeded... command %s' % command
+                print 'Max timeout exceeded... command %s' % command  #连接超时
                 # TODO: is timeout considered failure or next command still get executed
                 # now, next one got executed
                 return
         print 'Done!!! sent command: %s to %s' % (command, self.tello_ip)
 
     def _receive_thread(self):
+        #子线程接受无人机信息
         """Listen to responses from the Tello.
 
         Runs as a thread, sets self.response to whatever the Tello last returned.
@@ -58,9 +60,10 @@ class Tello:
         while True:
             try:
                 self.response, ip = self.socket.recvfrom(1024)
+                #返回值是（data,address）。其中data是包含接收数据的字符串，address是发送数据的套接字地址
                 print('from %s: %s' % (ip, self.response))
 
-                self.log[-1].add_response(self.response)
+                self.log[-1].add_response(self.response)  #日志最后一个状态更新
             except socket.error, exc:
                 print "Caught exception socket.error : %s" % exc
 
